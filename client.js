f3492a484cf397b2a11912a24fe4ccbf70671d9d
Harinder Labana <sunny.labana@gmail.com>
2022-06-14 12:40:30 -0400

    added a connection established message along with writing player name.

--- client.js
+++ client.js
@@ -10,9 +10,14 @@ const connect = function() {
   // interpret incoming data as text
   conn.setEncoding('utf8');
 
+  //print connection message and send Name to server
+  conn.on('connect', () => {
+    console.log('Connection established.');
+    conn.write('Name: LAB');
+  });
   //listening for data being broadcasted across server
   conn.on('data', (data) => {
-    console.log(data);
+    console.log('Timeout: ', data);
   });
 
   return conn;
