0e5037576adc65ef5e6c78e3fd2dff6ec5ef1fb3
Harinder Labana <sunny.labana@gmail.com>
2022-06-15 08:52:30 -0400

    added keybindings for movement

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 const net = require('net');
 
 // establishes a connection with the game server
-const connect = function() {
+const connect = function () {
   const conn = net.createConnection({
     host: 'localhost',
     port: 50541,
--- input.js
+++ input.js
@@ -1,5 +1,8 @@
+let connection;
+
 //setup the user input
-const setupInput = function() {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -9,9 +12,31 @@ const setupInput = function() {
 };
 
 //handle ctrl + c user input for exiting program
-const handleUserInput = function(data) {
+const handleUserInput = function (data) {
   if (data === '\u0003') {
     process.exit();
+  }
+  // console.log(data);
+
+  if (data === 'w') {
+    setInterval(() => {
+      connection.write('Move: up');
+    }, 50);
+  }
+  if (data === 'a') {
+    setInterval(() => {
+      connection.write('Move: left');
+    }, 50);
+  }
+  if (data === 's') {
+    setInterval(() => {
+      connection.write('Move: down');
+    }, 50);
+  }
+  if (data === 'd') {
+    setInterval(() => {
+      connection.write('Move: right');
+    }, 50);
   }
 };
 
